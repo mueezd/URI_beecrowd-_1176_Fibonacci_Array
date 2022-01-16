a232a394d5f217b444241481ca5a0617231d3f18
DeEpRo <mueezrz@gmail.com>
2022-01-16 14:55:04 +0600

    Add project files.

---
+++ URI_beecrowd _1176_Fibonacci_Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace URI_beecrowd__1176_Fibonacci_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long n, first = 0, second = 1, next = 0, c;
+            int i, j;
+
+            j = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= j; i++, first = 0, second = 1)
+            {
+                
+                n = long.Parse(Console.ReadLine());
+                n = n + 1;
+                for (c = 0; c < n; c++)
+                {
+                    if (c <= 1) next = c;
+                    else
+                    {
+                        next = first + second;
+                        first = second;
+                        second = next;
+                    }
+                }
+                Console.WriteLine("Fib({0}) = {1}", n - 1, next);
+            }
+        }
+    }
+ }
+
